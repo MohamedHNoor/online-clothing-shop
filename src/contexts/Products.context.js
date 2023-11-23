c2bf3e8bac41e8176c8f451871fe31263d2fb580
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-11-23 07:47:32 +0200

    Create products context and add shop products data

---
+++ src/contexts/Products.context.js
@@ -0,0 +1,15 @@
+import { createContext, useState } from 'react';
+import PRODUCTS from '../shop-data.json';
+export const productsContext = createContext({
+  products: [],
+});
+
+export const productsProvider = ({ children }) => {
+  const [products, setProducts] = useState(PRODUCTS);
+  const value = { products };
+  return (
+    <productsContext.Provider value={value}>
+      {children}
+    </productsContext.Provider>
+  );
+};
