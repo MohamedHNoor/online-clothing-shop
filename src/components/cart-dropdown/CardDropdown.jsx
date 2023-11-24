5cb1aee6d3159c372ab989f8c4c57e06f71c3303
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-11-24 16:28:13 +0200

    Modify cart dropdown

--- src/components/cart-dropdown/CardDropdown.jsx
+++ src/components/cart-dropdown/CardDropdown.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Button from '../button/Button';
+import CartItem from '../cart-item/CartItem';
+import { CartContext } from '../../contexts/Cart.context';
 import './cart-dropdown.styles.scss';
 
 const CardDropdown = () => {
+  const { cartItems } = useContext(CartContext);
   return (
     <div className='cart-dropdown-container'>
-      <div className='cart-items'></div>
+      <div className='cart-items'>
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+      </div>
       <Button>GO TO CHECKOUT</Button>
     </div>
   );
