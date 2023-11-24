6b32569df858811d63a907a299160a59e7b6a12f
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-11-24 16:28:55 +0200

    Add count functionality to cart-icon

--- src/components/cart-icon/CartIcon.jsx
+++ src/components/cart-icon/CartIcon.jsx
@@ -4,14 +4,14 @@ import CartImg from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss';
 
 const CartIcon = () => {
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
 
   const toggleCart = () => setIsCartOpen(!isCartOpen);
 
   return (
     <div className='cart-icon-container' onClick={toggleCart}>
       <img src={CartImg} alt='shopping bag' className='shopping-icon' />
-      <span className='item-count'>0</span>
+      <span className='item-count'>{cartCount}</span>
     </div>
   );
 };
