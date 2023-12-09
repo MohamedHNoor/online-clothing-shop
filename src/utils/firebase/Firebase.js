48f6d7ab83f79fba54f646eca2124b5de0592219
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-12-09 17:36:12 +0200

    Created a function to add collections and document data to firebase database

--- src/utils/firebase/Firebase.js
+++ src/utils/firebase/Firebase.js
@@ -8,7 +8,14 @@ import {
   signOut,
   onAuthStateChanged,
 } from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  writeBatch,
+} from 'firebase/firestore';
 
 // Firebase configuration
 const firebaseConfig = {
@@ -36,6 +43,23 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 
 // fireStore database
 export const db = getFirestore();
+
+// add data collection to fireStore db
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = collection(db, collectionKey);
+  const batch = writeBatch(db);
+
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object);
+  });
+
+  await batch.commit();
+  console.log('done');
+};
 
 // create user
 export const createUserDocumentFromAuth = async (
